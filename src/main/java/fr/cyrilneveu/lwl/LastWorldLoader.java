9db9b95aab16792eea322f3310040d14122dd481
Cyril <kassixlom@gmail.com>
2021-12-02 11:38:07 +0100

    Random update

--- src/main/java/fr/cyrilneveu/lwl/LastWorldLoader.java
+++ src/main/java/fr/cyrilneveu/lwl/LastWorldLoader.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.Mod;
 public class LastWorldLoader
 {
     public static final String MOD_ID = "lwl";
+    public static final String MINECRAFT_VERSION = "1.16.5";
 
     public LastWorldLoader() {
         MinecraftForge.EVENT_BUS.register(this);
